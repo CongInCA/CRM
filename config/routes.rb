ea2e49f7eb3991b4e5821adf6b3e98db338cad5c
CongInCA <captainchenn@163.com>
2024-03-04 11:52:40 -0600

    U6

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,21 @@ Rails.application.routes.draw do
   ActiveAdmin.routes(self)
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  resources :customers do
+    collection do
+      get 'missing_email', to: 'customers#missing_email'
+      get 'alphabetized', to: 'customers#alphabetized'
+    end
+  end
+
+
+  # root 'customers#index', as: 'customers_root'
+  root 'customers#index'
+
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
+
 
   # Defines the root path route ("/")
   # root "posts#index"
